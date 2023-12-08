cc4e3fbfef1fa83ac9ecdf9eefbf56907737111c
chavansanket <112459383+chavansanket@users.noreply.github.com>
2023-12-08 17:52:05 +0530

    project updated

--- to-doList_app/todoapp/src/App.js
+++ to-doList_app/todoapp/src/App.js
@@ -1,29 +1,24 @@
 
-import Navbar from './components/NavBar';
+// import React, { useState } from 'react';
+// import Navbar from './components/NavBar';
+// import FirstComponent from './components/FirstComponent';
+// import SecondComponent from './components/SecondComponet';
+
+import Todo from "./components/Todo";
 
 function App() {
-  let x=" great stack";
-  let array=[1,2,3,4,5];
-  const eventHan =()=>{
-    console.log("hi");
-  }
+  // const [x,setx] = useState(0); //1
+
+  
+
+
   return (
     <div>
-      Learning
-      {x}
-      {
-        array.map((arr)=>{
-          return <><h4 key={arr}>{arr}</h4></>
-        })
-      }
-      <button onClick={eventHan}>hi</button>
-      <br/>
-      <br/>
-      <img
-        src="https://i.imgur.com/MK3eW3Am.jpg"
-        alt="Katherine Johnson"
-      />
-
+      <Todo/>
+      
+      {/* <FirstComponent dat={x} fn={setx}/> //1 */}
+      {/* <Navbar/> */}
+      {/* <SecondComponent/> */}
     </div>
   );
 }
